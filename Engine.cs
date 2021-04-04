13906928e72a9d130f0c75b899062ea3e83d570d
Szymon Scholz <szymonscholz@gmail.com>
2021-04-05 00:10:04 +0200

    Lekko zbugowany, historia nie działa

---
+++ Engine.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    static class Engine
+    {
+        public static string Gracz1, Gracz2, aktualnygracz = "O";
+        static Random random = new Random();
+        public static int tura = 0, wynikgracz1 = 0, wynikgracz2 = 0;
+        public static int[,] plansza = new int[3, 3];
+
+
+        public static void NowaGra()
+        {
+            // Losuj znaki
+            if (random.Next() % 2 == 0)
+            {
+                Gracz1 = "X";
+                Gracz2 = "O";
+            }
+            else
+            {
+                Gracz1 = "O";
+                Gracz2 = "X";
+            }
+
+            // czyść planszę
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    plansza[i, j] = 0;
+
+            // zeruj tury
+            tura = 0;
+            aktualnygracz = "O";
+        }
+
+        public static void ZmianaTrybu()
+        {
+            wynikgracz1 = 0;
+            wynikgracz2 = 0;
+        }
+
+        public static void Wstaw(int x, int y)
+        {
+            if (plansza[x, y] != 0)
+                return;
+            plansza[x, y] = aktualnygracz == "O" ? 1 : -1;
+            var wynik = CzyWygrana();
+            if (wynik == 0)
+                NastepnaTura();
+            else
+            {
+                if (wynik == 1)
+                    _ = Gracz1 == "O" ? wynikgracz1++ : wynikgracz2++;
+                if (wynik == -1)
+                    _ = Gracz1 == "X" ? wynikgracz1++ : wynikgracz2++;
+            }
+
+            Console.WriteLine("\nStan planszy:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($" {plansza[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        private static void NastepnaTura()
+        {
+            aktualnygracz = aktualnygracz == "O" ? "X" : "O";
+            tura++;
+        }
+
+        public static int CzyWygrana()
+        {
+            int przekatna1 = 0, przekatna2 = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int wiersz = 0, kolumna = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    // zbieranie danych
+                    wiersz += plansza[i, j];
+                    kolumna += plansza[j, i];
+                    if (i == j)
+                        przekatna1 += plansza[i, j];
+                    if (i + j == 2)
+                        przekatna2 += plansza[i, j];
+                }
+
+                if (kolumna == 3 || wiersz == 3)
+                    return 1; // O
+                if (kolumna == -3 || wiersz == -3)
+                    return -1;  // X
+            }
+            if (przekatna1 == 3 || przekatna2 == 3)
+                return 1; // O
+            if (przekatna1 == -3 || przekatna2 == -3)
+                return -1; // X
+
+            return 0; // brak wygranej
+        }
+
+        public static int Bot() // ma zwracać int - kwadrat do zaznaczenia
+        {
+            var cele = Skaner();
+            int min = 0, max = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (cele[i, j] < min)
+                        min = cele[i, j];
+                    if (cele[i, j] > max)
+                        max = cele[i, j];
+                }
+            }
+
+            var moje = aktualnygracz == "O" ? 1 : -1;
+
+            // atakuj - jeśli wygrana
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (moje == 1 && max == 2)
+                    {
+                        if (cele[i, j] == max)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                    if (moje == -1 && min == -2)
+                    {
+                        if (cele[i, j] == min)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                }
+            }
+
+            // Blokuj
+            for (int i = 0; i < 3; i++)
+            {
+                // blokuj pewne wygrane
+                for (int j = 0; j < 3; j++)
+                {
+                    if (moje == -1 && max == 2)
+                    {
+                        if (cele[i, j] == max)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                    if (moje == 1 && min == -2)
+                    {
+                        if (cele[i, j] == min)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                }
+                // byle gdzie
+                for (int j = 0; j < 3; j++)
+                {
+                    if (moje == -1 && max == 1)
+                    {
+                        if (cele[i, j] == max)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                    if (moje == 1 && min == -1)
+                    {
+                        if (cele[i, j] == min)
+                        {
+                            Wstaw(i, j);
+                            return i * 3 + j;
+                        }
+                    }
+                }
+
+            }
+
+            // random
+            while (true)
+            {
+                try
+                {
+                    int i = random.Next() % 3, j = random.Next() % 3;
+                    Wstaw(i,j);
+                    return i * 3 + j;
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+
+        private static int[,] Skaner()
+        {
+            int przekatna1 = 0, przekatna2 = 0;
+            int[,] target = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                int wiersz = 0, kolumna = 0;
+
+                // zbieranie danych - kolumny i wiersze
+                for (int j = 0; j < 3; j++)
+                {
+                    // zbieranie danych
+                    wiersz += plansza[i, j];
+                    kolumna += plansza[j, i];
+                    if (i == j)
+                    {
+                        przekatna1 += plansza[i, j];
+                    }
+                    if (i + j == 2)
+                    {
+                        przekatna2 += plansza[i, j];
+                    }
+                }
+
+                // uzupełnianie danych w target, namierzamy tylko puste miejsca
+                for (int j = 0; j < 3; j++)
+                {
+                    // szukaj pustego miejsca i wpisz w nie odpowiednie wartości
+                    if (plansza[i, j] == 0)
+                        if (Math.Abs(target[i, j]) < Math.Abs(wiersz))
+                            target[i, j] = wiersz;
+                    if (plansza[j, i] == 0)
+                        if (Math.Abs(target[j, i]) < Math.Abs(kolumna))
+                            target[j, i] = kolumna;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                    {
+                        if (plansza[i, j] == 0)
+                            if (Math.Abs(target[i, j]) <= Math.Abs(przekatna1))
+                                target[i, j] = przekatna1;
+                    }
+                    if (i + j == 2)
+                    {
+                        if (plansza[i, j] == 0)
+                            if (Math.Abs(target[i, j]) <= Math.Abs(przekatna2))
+                                target[i, j] = przekatna2;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nPotencjalne cele:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($" {target[i, j]} ");
+                }
+                Console.Write("\n");
+            }
+
+            return target;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,215 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool MonitorTrybJednoosobowy;
+
         public MainWindow()
         {
             InitializeComponent();
+            MonitorTrybJednoosobowy = (bool)PoleTrybJednoosobowy.IsChecked;
+        }
+
+        public void UIRefresh()
+        {
+            PoleTura.Content = Engine.tura;
+            // Zmień stringi w wynikach
+
+            if (PoleTrybJednoosobowy.IsChecked == true)
+            {
+                PoleGracz1.Content = "Gracz";
+                PoleGracz2.Content = "Komputer";
+            }
+            else
+            {
+                PoleGracz1.Content = "Gracz 1";
+                PoleGracz2.Content = "Gracz 2";
+            }
+
+            PoleGracz1Wynik.Content = Engine.wynikgracz1;
+            PoleGracz2Wynik.Content = Engine.wynikgracz2;
+
+            if (Engine.aktualnygracz == Engine.Gracz1)
+            {
+                PoleGracz1.Background = Brushes.LightSeaGreen;
+                PoleGracz2.Background = null;
+            } else
+            {
+                PoleGracz2.Background = Brushes.LightSeaGreen;
+                PoleGracz1.Background = null;
+            }
+
+            if (Engine.plansza[0,0] != 0)
+                Matrix1.Content = Engine.plansza[0, 0] == 1 ? "O" : "X";
+            if (Engine.plansza[0, 1] != 0)
+                Matrix2.Content = Engine.plansza[0, 1] == 1 ? "O" : "X";
+            if (Engine.plansza[0, 2] != 0)
+                Matrix3.Content = Engine.plansza[0, 2] == 1 ? "O" : "X";
+            if (Engine.plansza[1, 0] != 0)
+                Matrix4.Content = Engine.plansza[1, 0] == 1 ? "O" : "X";
+            if (Engine.plansza[1, 1] != 0)
+                Matrix5.Content = Engine.plansza[1, 1] == 1 ? "O" : "X";
+            if (Engine.plansza[1, 2] != 0)
+                Matrix6.Content = Engine.plansza[1, 2] == 1 ? "O" : "X";
+            if (Engine.plansza[2, 0] != 0)
+                Matrix7.Content = Engine.plansza[2, 0] == 1 ? "O" : "X";
+            if (Engine.plansza[2, 1] != 0)
+                Matrix8.Content = Engine.plansza[2, 1] == 1 ? "O" : "X";
+            if (Engine.plansza[2, 2] != 0)
+                Matrix9.Content = Engine.plansza[2, 2] == 1 ? "O" : "X";
+        }
+
+        private void Button_Click_Nowa_Gra(object sender, RoutedEventArgs e)
+        {
+            Engine.NowaGra();
+
+            // zeruj wyniki, jeśli zmienił się tryb gry
+
+            if (MonitorTrybJednoosobowy != PoleTrybJednoosobowy.IsChecked)
+            {
+                MonitorTrybJednoosobowy = (bool)PoleTrybJednoosobowy.IsChecked;
+                Engine.ZmianaTrybu();
+                PoleGracz1Wynik.Content = Engine.wynikgracz1;
+                PoleGracz2Wynik.Content = Engine.wynikgracz2;
+            }
+
+            if (MonitorTrybJednoosobowy && Engine.Gracz2 == "O")
+            {
+                Engine.Bot();
+            }
+
+            Matrix1.Content = "";
+            Matrix2.Content = "";
+            Matrix3.Content = "";
+            Matrix4.Content = "";
+            Matrix5.Content = "";
+            Matrix6.Content = "";
+            Matrix7.Content = "";
+            Matrix8.Content = "";
+            Matrix9.Content = "";
+
+            UIRefresh();
+        }
+
+        private void Button_Click_Historia(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Matrix1.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix1.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(0, 0);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }  
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Matrix2.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix2.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(0, 1);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (Matrix3.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix3.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(0, 2);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (Matrix4.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix4.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(1, 0);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            if (Matrix5.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix5.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(1, 1);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            if (Matrix6.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix6.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(1, 2);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            if (Matrix7.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix7.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(2, 0);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            if (Matrix8.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix8.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(2, 1);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            if (Matrix9.Content.Equals("") && Engine.CzyWygrana() == 0)
+            {
+                Matrix9.Content = Engine.aktualnygracz;
+
+                Engine.Wstaw(2, 2);
+                if (MonitorTrybJednoosobowy && Engine.CzyWygrana() == 0)
+                    Engine.Bot();
+                UIRefresh();
+            }
         }
     }
 }
